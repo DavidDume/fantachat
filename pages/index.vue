ecf66f2274de72623fd849794d6c016b143512d0
David Dume <dumedavid11@gmail.com>
2023-11-03 12:47:26 +0100

    syntax

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Hero />
     <div class="w-[70%] mx-auto p-2 flex items-center" id="chat">
         <p class="leading-7">
-            Esempi di domande: <br>
+            Esempi di domande:<br>
             Chi dovrei schierare tra Immobile e Lukaku questa settimana?<br>
             Pensi che sia il momento giusto per vendere Chiesa?<br>
             Come dovrei gestire il mio budget per equilibrare la mia squadra?
